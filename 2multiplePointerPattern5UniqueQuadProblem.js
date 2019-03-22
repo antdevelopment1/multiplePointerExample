f77b91e0165fe3c19dcdbbea60f53707dadb00b3
April Copes <antdevelopment1@gmail.com>
2019-03-21 23:12:18 -0400

    unique pattern

---
+++ 2multiplePointerPattern5UniqueQuadProblem.js
@@ -0,0 +1,11 @@
+// Given an array of numbers, write a function that returns an array that has all duplicate elements removed and
+// is sorted from least to greatest value.
+
+// Examples
+// uniqueSort([1, 2, 4, 3]) ➞ [1, 2, 3, 4]
+// uniqueSort([1, 4, 4, 4, 4, 4, 3, 2, 1, 2]) ➞ [1, 2, 3, 4]
+// uniqueSort([6, 7, 3, 2, 1]) ➞ [1, 2, 3, 6, 7]
+
+// ================================
+// O(N^2) Quadratic Solution Edabit
+// ================================
