157a8ae4501e2582dbabfd453001374c090c9aa7
April Copes <antdevelopment1@gmail.com>
2019-03-21 23:08:26 -0400

    Adds average algorithm

---
+++ 2multiplePointerPattern8AverageProblem.js
@@ -0,0 +1,8 @@
+// Given a sorted array of integers and a target average, determine if there is a pair of values in the array where the average of 
+// the pair equals the target value. There may be more than 1 pair that matches the average target
+
+// Examples
+// ([1,2,3], 2.5) = true
+// ([1,3,3,5,6,7,10,12,19], 8) = true
+// ([-1,0,3,4,5,6], 4.1) = false
+// ([], 4) = false
