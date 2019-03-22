26e51621f10f9fa1d4a4a7cada6004d6d7adac4c
April Copes <antdevelopment1@gmail.com>
2019-03-21 23:04:01 -0400

    Adds algorithm

---
+++ 2multiplePointerPattern1QuadProblem.js
@@ -0,0 +1,7 @@
+// Write a function called sumZero which accepts a sorted array of integers. The function should find the 
+// first pair where the sum is 0. Return an array that includes both values that sum to zero or undefined 
+// if a pair does not exsist.
+// =================================================
+// Solve with a runtime of O(N^2) Quadratic Solution
+// =================================================
+[-3,-2,-1,0,3,4,4,5] = [-3, 3];
