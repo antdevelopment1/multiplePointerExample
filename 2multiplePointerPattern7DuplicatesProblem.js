e093457ad3f5a97555b718ae64ff8a24740a886e
April Copes <antdevelopment1@gmail.com>
2019-03-21 23:10:19 -0400

    Adds duplicates algorithm

---
+++ 2multiplePointerPattern7DuplicatesProblem.js
@@ -0,0 +1,8 @@
+// Create a function which accepts a variable number of arguments, and checks whether there are any duplicates 
+// among the arguments passed in.
+// This can solved using either the freq pattern or multiple pointer pattern
+
+// Example
+// (1,2,3) = false
+// (1,2,2) = true
+// ('a', 'b', 'c', 'a') = true
