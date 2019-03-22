0037ba5c287309655dfbc55942ab3bb48919c781
April Copes <antdevelopment1@gmail.com>
2019-03-21 23:11:45 -0400

    unique quad algorithm

---
+++ 2multiplePointerPattern5UniqueQuadPseudoCode.js
@@ -0,0 +1,33 @@
+// Given an array of numbers, write a function that returns an array that has all duplicate elements removed and
+// is sorted from least to greatest value.
+// Given an array of numbers, write a function that returns an array that has all duplicate elements removed and
+// is sorted from least to greatest value.
+
+// Examples
+// uniqueSort([1, 2, 4, 3]) ➞ [1, 2, 3, 4]
+// uniqueSort([1, 4, 4, 4, 4, 4, 3, 2, 1, 2]) ➞ [1, 2, 3, 4]
+// uniqueSort([6, 7, 3, 2, 1]) ➞ [1, 2, 3, 6, 7]
+
+// ================================
+// O(N^2) Quadratic Solution Edabit
+// ================================
+// Create an empty function that accepts an array
+
+    // Check if the length of the array is greater than 0. If its't return []
+    
+
+    // We sort the array from smallest to biggest
+    
+
+    // We create an empty array
+    
+    // We run a for loop through the length of the sorted array
+   
+        // Save the current index we are looping over in an array
+      
+        // We check if the value we are looping through is inside our array or not
+     
+            // If the value is not already inside the array then we will push the value into our new array
+            
+    // We return the new sorted array with duplicates removed
+    
