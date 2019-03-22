e3860bee40fd9d9d87eb520b8399540ad181866b
April Copes <antdevelopment1@gmail.com>
2019-03-21 23:06:58 -0400

    Adds algorithm

---
+++ 2multiplePointerPattern9SussecProblem.js
@@ -0,0 +1,7 @@
+// Create a function which takes two strings and checks whether the characters in the first string form a subsequence of the characters in the second string. The function should check whether the characters in the first string appear somewhere in the second string, without their order changing.
+
+// // Examples
+// ('hello', 'hello world') = true
+// ('sing', 'sting') = true
+// ('abc', 'abracadabra') = true
+// ('abc', 'acb') = false
