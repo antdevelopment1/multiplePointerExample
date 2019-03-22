176a10a4caaf74fb23cb6e6b380fc100a4f6868e
April Copes <antdevelopment1@gmail.com>
2019-03-21 23:07:53 -0400

    algorithm for pointer pattern average

---
+++ 2multiplePointerPattern8AveragePseudoCode.js
@@ -0,0 +1,30 @@
+// Given a sorted array of integers and a target average, determine if there is a pair of values in the array where the average of 
+// the pair equals the target value. There may be more than 1 pair that matches the average target
+
+// Create an empty function that accepts an array and an interger as a target value
+
+    // Check if the length of the array is 0
+    
+        // If it is we return false
+        
+    // Start a pointer at the begining and save it in a variable
+ 
+    // Start a pointer at the end and save it to a variable
+   
+
+    // Create a while loop that runs while start is less than end
+    
+        // We save the value of the first pointer plus the second pointer divided by 2 in a variable giving
+        // us the vaerage
+      
+        // We check if that value is equal to our target variable
+      
+            // If it is we return true
+           
+        // Otherwise is the average is less than the target we increment our first pointer
+        
+        // Otherwise we will decrement our second pointer 
+        
+    // If we make to the middle of the array and can't find our target average based on adding the sum
+    // of the two pointers as we loop through the array we return false
+    
