8e1eb9b70f50f53ea578c0cf9fd7bd332ddb8d4b
April Copes <antdevelopment1@gmail.com>
2019-03-21 23:06:39 -0400

    Adds algorithm

---
+++ 2multiplePointerPattern9SussecPseudoCode.js
@@ -0,0 +1,35 @@
+// Create a function which takes two strings and checks whether the characters in the first string form a 
+// subsequence of the characters in the second string. The function should check whether the characters in 
+// the first string appear somewhere in the second string, without their order changing.
+
+// Examples
+// ('hello', 'hello world') = true
+// ('sing', 'sting') = true
+// ('abc', 'abracadabra') = true
+// ('abc', 'acb') = false
+
+// Create a function that accepts two strings
+
+    // Create a variable for i and start it at 0
+   
+    // Create a empty variable for j and start it at 0
+    
+
+    // If no string is passed we return true
+  
+        // I'm unsure if I feel this should return true. If there is no first string I would think we return false if no 
+        // first string was passed
+       
+
+    // Run a while loop while j is less than the second string
+   
+        // We check if the first letter of each word matches each other
+    
+            // If they do we will increment i by 1
+           
+        // If the count for i makes it all the way to the end of string1 then we return true
+        
+        // We increment j as we go through the loop to check the next letter agaianst str1[i]
+       
+    // If we finish the loop before we can return true we will return false
+   
